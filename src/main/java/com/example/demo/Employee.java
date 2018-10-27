b6621e01cf3fb1c1333349482f237e3280e94eb5
ashleigh-knight1 <ashleigh.knight.21@gmail.com>
2018-10-27 18:32:45 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstname;
+    private String lastname;
+    private int ssn;
+    private int DOB;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(int DOB) {
+        this.DOB = DOB;
+    }
+}
